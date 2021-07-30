32835e95bc2a75b5d821a8e62b317d65655dc468
Gareth Whitley <me@garethwhitley.online>
2021-07-30 20:07:05 +0100

    changed height of feedback card area

--- resources/views/livewire/feedback/feedback-index.blade.php
+++ resources/views/livewire/feedback/feedback-index.blade.php
@@ -1,9 +1,33 @@
 <div class="grid md:gap-3 mx-auto w-screen h-full md:w-[689px] md:mt-3 grid-cols-1 grid-rows-8 md:grid-rows-4 md:grid-cols-1 lg:w-full lg:grid-cols-4 lg:grid-rows-1 bg-blue-50 ">
-    <div class="flex justify-start mx-auto bg-blue-50 col-span-1 h-[100px] md:h-auto md:gap-[10px] md:flex-row lg:flex-col lg:mt-3 lg:gap-5">
-        <div class="flex  p-5 bg-headerImage bg-cover w-screen md:flex-col-reverse md:rounded-xl md:w-[223px] md:h-[178px] lg:w-[255px] lg:h-[137px] ">
-            <div class="text-white">
+    <div class="flex justify-start mx-auto bg-blue-50 col-span-1 h-[72px] md:h-auto md:gap-[10px] md:flex-row lg:flex-col lg:mt-3 lg:gap-5">
+        <div class="flex p-3 bg-headerImage bg-cover w-screen md:flex-col-reverse md:rounded-xl md:w-[223px] md:h-[178px] lg:w-[255px] lg:h-[137px] ">
+            <div class="text-white w-full mb-3 md:mb-0">
                 <span class="font-bold text-xl">Frontend Mentor</span><br>
                 <span>Feedback Board</span>
+            </div>
+            <div class="float-right mt-3 mr-5 md:hidden">
+                <x-menu>
+                    <x-slot name="trigger">
+                        <button>
+                            <img src="{{asset('storage/images/shared/mobile/icon-hamburger.svg')}}">
+                        </button>
+                    </x-slot>
+                    <x-slot name="close">
+                        <button>
+                            <img src="{{asset('storage/images/shared/mobile/icon-close.svg')}}">
+                        </button>
+                    </x-slot>
+                    <div class="absolute text-left right-0 z-50 w-1/2 h-full bg-gray-100 top-[72px]">
+                        <div class="grid grid-cols-1 grid-rows-3 gap-5 p-3 row-span-1">
+                            <div class="bg-white rounded-xl justify-center">
+                                <livewire:generic.category-button :categories="$categories" />
+                            </div>
+                            <div class="bg-white rounded-xl justify-center text-mainText pb-5">
+                                <livewire:sidebar.roadmap />
+                            </div>
+                        </div>
+                    </div>
+                </x-menu>
             </div>
         </div>
         <div class="bg-white  hidden md:block md:w-[223px] md:h-[178px] lg:w-[255px] lg:h-[166px] md:rounded-xl">
@@ -19,7 +43,7 @@
         </div>
         <div class="rounded-xl row-start-2 row-span-4 col-span-4 grid  lg:grid-rows-8 grid-cols-1 lg:h-full w-full">
             @if($feedback->count() > 0)
-                <div class="row-span-12 rounded-xl h-full md:mt-20 lg:mt-0">
+                <div class="row-span-12 rounded-xl h-screen md:mt-20 lg:mt-0">
                         @foreach($feedback as $f)
                             <livewire:feedback.feedback-card :key="$f->id" :feedback=$f />
                         @endforeach    
